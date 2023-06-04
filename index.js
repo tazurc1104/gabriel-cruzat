d32ae64239be9f359a32120250eaab11fc1ae026
tazurc1104 <127004476+tazurc1104@users.noreply.github.com>
2023-06-04 17:05:54 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const phoneNav = document.querySelector(".phone-nav-links");
+var opened = false;
+
+function myFunction(x) {
+    
+    
+    if (opened === true){
+      phoneNav.style.opacity = 0;
+      x.classList.toggle("change");
+      opened = false;
+    }
+    else {
+      x.classList.toggle("change");
+      phoneNav.style.opacity = 1;
+      opened = true;
+    }
+  }
